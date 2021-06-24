ddf3e63fe46c959291588fe90d76803fe7c2079c
che711 <che711@mail.tu>
2021-06-24 10:51:29 +0300

    add requirements.txt

--- test_api/tests_api.py
+++ test_api/tests_api.py
@@ -1,17 +1,13 @@
+from json import dumps
 import requests
-from json import loads, dumps
 from test_api import api_urls
 from test_api.headers import HEADERS
 
+
 def test_user_crud(user_data):
-    user_response = requests.post(
-        url=api_urls.USER, headers=HEADERS, data=dumps(user_data)
-    )
+    user_response = requests.post(url=api_urls.USER, headers=HEADERS, data=dumps(user_data))
     assert user_response.status_code == 200
-    get_user_response = requests.get(
-        url=api_urls.USER_NAME.format(username=user_data["username"]),
-        headers=HEADERS
-    )
+    get_user_response = requests.get(url=api_urls.USER_NAME.format(username=user_data["username"]),headers=HEADERS)
     assert get_user_response.status_code == 200, 'not OK'
     assert get_user_response.json()["id"] == user_data["id"]
     update_data = {"email": "updated@sad.com"}
@@ -20,23 +16,23 @@ def test_user_crud(user_data):
         url=api_urls.USER_NAME.format(username=user_data["username"]),
         headers=HEADERS,
         data=dumps(updated_user_data)
-    )
+        )
     assert updated_response.status_code == 200
     get_updated_user = requests.get(
         url=api_urls.USER_NAME.format(username=user_data["username"]),
         headers=HEADERS
-    )
+        )
     assert get_updated_user.status_code == 200, 'not OK v.2'
     assert get_updated_user.json()["email"] == update_data["email"]
     delete_user_response = requests.delete(
         url=api_urls.USER_NAME.format(username=user_data["username"]),
         headers=HEADERS
-    )
+        )
     assert delete_user_response.status_code == 200
     get_deleted_user = requests.get(
         url=api_urls.USER_NAME.format(username=user_data["username"]),
         headers=HEADERS
-    )
+        )
     assert get_deleted_user.status_code == 404
 
 
