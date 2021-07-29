34cc25f3a18276177e0ba00d9dae38edaa9414a8
che711 <che711@mail.ru>
2021-07-29 14:13:46 +0300

    fix conftest.py

--- conftest.py
+++ conftest.py
@@ -13,8 +13,8 @@ def pytest_addoption(parser):
 def browser(request):
     user_language = request.config.getoption("language")
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     options.add_argument("--headless")
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
     #browser.maximize_window()
     time.sleep(5)
